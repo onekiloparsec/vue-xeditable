9d81ab6cff7ae63f53f2a0b9fd67024cfdaef85b
Cédric Foellmi <cedric@onekilopars.ec>
2019-04-10 22:00:36 +0200

    Fixed config (problem wit axios)

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   configureWebpack: config => {
     config.externals = {
+      ...config.externals,
       'lodash': 'lodash',
-      '_': 'lodash',
-      'axios': 'axios'
+      '_': 'lodash'
     }
   },
   css: {
